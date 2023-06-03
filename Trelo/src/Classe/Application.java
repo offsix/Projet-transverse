0079e8ffa65f9b1b76c37265dab4b8209597a294
Abdoulaye <abdoulaye.wagne@universite-paris-saclay.fr>
2023-06-04 01:54:31 +0200

    aaaaaaaa

--- Trelo/src/Classe/Application.java
+++ Trelo/src/Classe/Application.java
@@ -13,46 +13,7 @@ import Controller.*;
 
 public class Application {
 
-  // public static ModeleProjet[] creationProjets(int iNum) { // Fonction pour cr�er des projets
-  //   ModeleProjet[] tableauProjets = new ModeleProjet[iNum];
-
-  //   // Création des cartes et des membres
-  //   ModeleCartes[] tableauCartes = creationCarte(iNum);
-  //   ModeleMembre[] tableauMembres = new ModeleMembre[0];
-
-  //   for (int i = 0; i < iNum; i++) {
-  //     ModeleCartes[] cartesProjet = tableauCartes; // Utiliser le m�me tableau de cartes pour chaque projet
-  //     // Créer un projet avec les cartes et le propriétaire
-  //     ModeleProjet projet = new ModeleProjet("Mon projet", tableauCartes, tableauMembres, "Moi");
-  //     tableauProjets[i] = projet; // Ajouter le projet dans le tableau
-  //   }
-  //   return tableauProjets;
-  // }
-
-  // public static ModeleCartes[] creationCarte(int iNum) { // Fonction pour cr�er des Cartes
-  //   ModeleCartes[] tableauCartes = new ModeleCartes[iNum]; // Tableau pour stocker les cartes
-  //   // Liste des couleurs pr�d�finies
-  //   String[] couleurs = {
-  //       "#FBE0C3",
-  //       "#FFBB98",
-  //       "#7D8E95",
-  //       "#344648"
-  //   };
-
-  //   for (int j = 1; j <= iNum; j++) {
-  //     String titre = "Carte " + j;
-  //     LocalDate dateDebut = LocalDate.now();
-  //     String description = "Description de la carte " + j;
-  //     LocalDate dateLimite = LocalDate.now().plusDays(j);
-  //     String statut = "En cours";
-  //     String couverture = couleurs[(j - 1) % couleurs.length]; // S�lectionner une couleur de mani�re cyclique
-
-  //     ModeleCartes carte1 = new ModeleCartes(titre, dateDebut, description, dateLimite, statut, couverture);
-  //     tableauCartes[j - 1] = carte1; // Ajouter la carte dans le tableau
-  //   }
-  //   return tableauCartes;
-  // }
-
+ 
 /**
  * Initialise l'application en créant et initialisant différents objets, notamment:
  * un objet `ModeleAccueil`, un objet `VueAccueil`, un objet `VueInscription`,
@@ -70,15 +31,6 @@ public class Application {
     ModeleMembre test = new ModeleMembre("a","a","a","a","a@outlook.fr");
     modeleAccueil.ajouterMembre(test);
 
-    
-
-    // // Creer un modele d'ajouter une carte
-    // JFrame fenetreAjouteCarte = new JFrame("VueAjouterCarte");
-    // fenetreAjouteCarte.setSize(400, 500);
-    // fenetreAjouteCarte.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    // ModeleAjouterCarte ajoutCarte = new ModeleAjouterCarte();
-    // VueAjouterCarte vueAjouteCarte = new VueAjouterCarte(ajoutCarte);
-    // fenetreAjouteCarte.add(vueAjouteCarte);
 
     // Creer un modele d'un projet
     JFrame fenetreProjet = new JFrame("VueProjet");
@@ -92,26 +44,7 @@ public class Application {
     fenetreProjet.setVisible(true);
 
 
-    // Creer un modele d'un Petitprojet
-    // JFrame fenetrePetitProjet = new JFrame("VuepetitProjet");
-    // VuePetitProjet vuePetitProject = new VuePetitProjet(projet);
-    // fenetrePetitProjet.add(vuePetitProject);
-    // fenetrePetitProjet.setSize(300, 200);
-    // fenetrePetitProjet.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    // fenetrePetitProjet.add(vuePetitProject);
-    // // Creer un modele d'une carte
-    // JFrame fenetreCarte = new JFrame("VueCarte");
-    // fenetreCarte.setSize(300, 200);
-    // fenetreCarte.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-
-    // // Creer un modele d'un membre
-    // JFrame fenetrePetitMembre = new JFrame("VueMembre");
-    // fenetrePetitMembre.setSize(100, 150);
-    // fenetrePetitMembre.setResizable(false);
-    // VuePetitMembre vuePetitMembre = new VuePetitMembre(test);
-    // fenetrePetitMembre.add(vuePetitMembre);
-    // fenetrePetitMembre.setVisible(true);
-    // fenetrePetitMembre.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+    
 
   }
 }
--- Trelo/src/Modele/ModeleAccueil.java
+++ Trelo/src/Modele/ModeleAccueil.java
@@ -5,20 +5,40 @@ import java.util.ArrayList;
 public class ModeleAccueil {
 
 private ArrayList<ModeleMembre> membres;	
+
+	//contructeur avec l'arraylist de membre qui stockera tout les memebre inscrits
 	public ModeleAccueil()
 	{
 		this.membres = new ArrayList<ModeleMembre>();
 	}
+		/**
+	 * Adds the given member to the list of members.
+	 *
+	 * @param  membre the member to be added to the list
+	 */
+
 	public void ajouterMembre(ModeleMembre membre)
 	{
 		this.membres.add(membre);
 	}
+
+	/**
+	 * Iterates through the list of members and prints their pseudonyms.
+	 */
 	public  void listemembre() 
 	{
 		for (ModeleMembre modeleMembre : membres) {
 			System.out.println(modeleMembre.getPseudo());
 		}
 	}
+
+		/**
+	 * Returns a model member with the given name.
+	 *
+	 * @param  nommembre	the name of the member to retrieve
+	 * @return         	the model member with the given name, or null if not found
+	 */
+
 	public ModeleMembre getMembre(String nommembre){
 		for (ModeleMembre modeleMembre : membres) {
 			if(modeleMembre.getPseudo().equals(nommembre))
@@ -26,6 +46,14 @@ private ArrayList<ModeleMembre> membres;
 		}
 		return null;
 	}
+
+		/**
+	 * Check if a member exists in the database.
+	 *
+	 * @param  pseudo	the username of the member
+	 * @param  password	the password of the member
+	 * @return         	true if the member exists, false otherwise
+	 */
 	public boolean estunmembre(String pseudo,String password){
 		boolean resultat = false;
 		for (ModeleMembre modeleMembre : membres) {
